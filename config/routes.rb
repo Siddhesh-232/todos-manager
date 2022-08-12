9eea094c3874a0dd10b4a369ab50fbc0a8a7c988
Sidd <dandekarsiddhesh.02@gmail.com>
2022-08-13 03:01:56 +0530

    ERB Templating and Asset Pipeline

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # post "todos/create", to: "todos#create"
   # get "todos", to: "todos#index"
   # get "todos/:id", to: "todos#show"
+  get "/" => "home#index"
   resources :todos
   resources :users do
     collection do
