8a34147a082c71271fd6b1a112f5ba413abb08de
Sidd <dandekarsiddhesh.02@gmail.com>
2022-08-13 02:01:17 +0530

    Listing todo into formatted manner

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,7 @@
 # todos_controller.rb
 class TodosController < ApplicationController
   def index
-    render plain: Todo.all.to_a
+    render plain: Todo.order(:due_date).map {|todo| todo.to_pleasant_string}.join("\n")
     #render plain: "Hello, this is /todos!"
   end
 end
